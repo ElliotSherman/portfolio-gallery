1214194d5dc090d3132523b3ea3c872e89130eb5
elliot sherman <sheveryosef@gmail.com>
2022-08-01 15:59:25 +0300

    render portfolio cards dynamically

--- js/gallery.service.js
+++ js/gallery.service.js
@@ -18,19 +18,46 @@ const specialitiesDescriptions = [
     { title: 'Web Security', desc: LOREM_STR }
 ]
 
-var strNavLis = gNavLis.map(li => `<li class="nav-item">\n 
-    \t<a class="nav-link js-scroll-trigger" href="${li.href}">${li.txt}</a>\n</li> \n
-`).join('')
+const portfolioCards = [
+    { title: 'Threads', txt: 'Illustration', href: '#portfolioModal1', img: '01-thumbnail.jpg' },
+    { title: 'Explore', txt: 'Graphic Design', href: '#portfolioModal2', img: '02-thumbnail.jpg' },
+    { title: 'Finish', txt: 'Identity', href: '#portfolioModal3', img: '03-thumbnail.jpg' },
+    { title: 'Lines', txt: 'Branding', href: '#portfolioModal4', img: '04-thumbnail.jpg' },
+    { title: 'Southwest', txt: 'Website Design', href: '#portfolioModal5', img: '05-thumbnail.jpg' },
+    { title: 'Window', txt: 'Photography', href: '#portfolioModal6', img: '06-thumbnail.jpg' }
+]
 
 function getStrNavLis() {
-    return strNavLis
+    return strNavLis()
 }
 
- function getStrDescs(){
+function getStrDescs() {
     return strDescs()
- }
+}
 
- function strDescs(){
+function getStrPortfolioCards() {
+    return strPortfolioCards()
+}
+
+function strPortfolioCards() {
+    return portfolioCards.map(card => `        
+    <div class="col-md-4 col-sm-6 portfolio-item">\n
+    \t<a class="portfolio-link" data-toggle="modal" href="${card.href}">\n
+      \t<div class="portfolio-hover">\n
+        \t<div class="portfolio-hover-content">\n
+          \t<i class="fa fa-plus fa-3x"></i>\n
+        \t</div>\n
+      \t</div>\n
+      \t<img class="img-fluid" src="img/portfolio/${card.img}" alt="">\n
+    \t</a>\n
+    \t<div class="portfolio-caption">\n
+      \t<h4>${card.title}</h4>\n
+      \t<p class="text-muted">${card.txt}</p>\n
+    \t</div>\n
+    </div>\n`).join('')
+}
+
+function strDescs() {
     return specialitiesDescriptions.map(desc => ` <div class="col-md-4">\n
     \t<span class="fa-stack fa-4x">\n
       \t<i class="fa fa-circle fa-stack-2x text-primary"></i>\n
@@ -38,11 +65,27 @@ function getStrNavLis() {
     \t</span>\n
     \t<h4 class="service-heading">${desc.title}</h4>\n
     \t<p class="text-muted">${desc.desc}</p>\n
-    </div>\n` )
- }
+    </div>\n` ).join('')
+}
 
 function strNavLis() {
     return gNavLis.map(li => `<li class="nav-item">\n 
     \t<a class="nav-link js-scroll-trigger" href="${li.href}">${li.txt}</a>\n</li> \n
 `).join('')
 }
+
+// var str = `        
+// <div class="col-md-4 col-sm-6 portfolio-item">\n
+// \t<a class="portfolio-link" data-toggle="modal" href="${card.href}">\n
+//   \t<div class="portfolio-hover">\n
+//     \t<div class="portfolio-hover-content">\n
+//       \t<i class="fa fa-plus fa-3x"></i>\n
+//     \t</div>\n
+//   \t</div>\n
+//   \t<img class="img-fluid" src="img/portfolio/${card.img}" alt="">\n
+// \t</a>\n
+// \t<div class="portfolio-caption">\n
+//   \t<h4>${card.title}</h4>\n
+//   \t<p class="text-muted">${card.txt}</p>\n
+// \t</div>\n
+// </div>\n`
--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ console.log('Starting up controller');
 
 // for each link in the nav we will create a dynamic li
 
+// render nav links
 $('.navbar-nav').html(getStrNavLis())
-
+// render service descs
 $('[data-services="services-container"]').html(getStrDescs())
+// render portfolio gallery links
+$('[data-portfolio-cards="data-portfolio-cards"]').html(getStrPortfolioCards())
